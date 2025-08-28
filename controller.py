ab7de6743f437fcfdf814bff72ef2e40a46eb885
PaoLyn33 <paoo11tvxq@gmail.com>
2025-08-28 11:25:21 -0400

    tarea del punto 1 y punto 2

--- controller.py
+++ controller.py
@@ -1,21 +1,23 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
-# Configuración para PostgreSQL
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:proyecto@localhost:5432/postgres'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-# Modelo de ejemplo
 class Usuario(db.Model):
-    __tablename__ = 'usuarios'  # nombre de la tabla en Postgres
+
+    _tablename_ = 'usuarios'
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(100), nullable=False)
 
-# Crear las tablas en la BD
 with app.app_context():
     db.create_all()
 
